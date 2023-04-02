67644e43df8704c01292ed386e9cd0d112b45758
Neeraj <66212620+neeradnak@users.noreply.github.com>
2023-04-03 00:03:31 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import pandas as pd
+from PIL import Image
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
