5cdc333215ef29f52e97dd690b872710b5628fde
Neeraj <66212620+neeradnak@users.noreply.github.com>
2023-04-03 12:28:16 +0530

    Update file.py

--- file.py
+++ file.py
@@ -29,3 +29,18 @@ def add_bg_from_url():
      )
 
 add_bg_from_url()
+def user_input_features():
+
+    mat = st.selectbox('Material of Construction of Lug?',  ("SA 516 GR. 70",))
+    Ys= st.number_input('Yield Strength at Room Temperature( kg/mm² )')
+    N = st.number_input('Number Of Lugs',value=1,step=1)
+    Lb =st.number_input('Length Of Bottom Of Lug ( mm )')
+    Lc= st.number_input('Length Of Lug at Centerline of Hole ( mm )')
+    t = st.number_input('Thickness of Lug( mm )')
+    L= st.number_input('Distance of centerline of Hole from bottom of Lug( mm )')
+    dh =st.number_input('Diameter of Hole of Lug( mm )')
+    lf =st.number_input('Length of Weld Leg( mm )')
+    E= st.number_input('Weld Efficiency')
+    W =st.number_input('Weight on a Dishend ( Kg )')
+    IP =st.number_input('Impact factor')
+output = user_input_features()
