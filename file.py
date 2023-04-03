08cbd084549220af865ebdf7e9fef4adc696b45e
Neeraj <66212620+neeradnak@users.noreply.github.com>
2023-04-03 12:18:09 +0530

    Update file.py

--- file.py
+++ file.py
@@ -4,6 +4,14 @@ import pandas as pd
 from PIL import Image
 image = Image.open('L&T.png')
 st.set_page_config(page_title="LLD",layout="wide", page_icon=image)
+hide_menu_style="""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer { visibility : hidden;}
+    </style>
+    """
+st.markdown(hide_menu_style,unsafe_allow_html=True)
+
 
 
 def add_bg_from_url():
